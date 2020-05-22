aea2bb425df435c1cd6ff256b399beac66bcdae0
Monica Li <monica.yc.li@gmail.com>
2020-05-22 10:40:59 -0400

    update main.py with new examples

--- main.py
+++ main.py
@@ -8,8 +8,11 @@ from dataset import Dataset
 from model import Model
 
 def main():
-    dataset = Dataset(scale=1.0, shuffle=False)
-    model = Model()
+    # dataset = Dataset(scale=1.0, shuffle=False)
+    dataset = Dataset(scale=1.0, shuffle=False, data_dir="./data/images_rao_128x128",
+                      rf1_x=16, rf1_y=16, rf1_offset_x=8, rf1_offset_y=8,
+                      rf1_layout_x=15, rf1_layout_y=15, gauss_mask_sigma=1.0)
+    model = Model(iteration=100, dataset=dataset)
     model.train(dataset)
 
     model.save("saved")
@@ -17,16 +20,34 @@ def main():
     if not os.path.exists("result"):
         os.mkdir("result")
     
-    for i in range(32):
-        u1 = model.Us[1][:,i].reshape(16,16)
+    # receptive fields of neurons in level 1 center module
+    for i in range(model.level1_module_size):
+        u1 = model.Us[model.level1_module_n//2][:,i].reshape((dataset.rf1_size[1],dataset.rf1_size[0]))
         u1 = cv2.resize(u1, None, fx=4, fy=4, interpolation=cv2.INTER_NEAREST)
         imageio.imwrite("result/u1_{:0>2}.png".format(i), u1)
     
-    for i in range(128):
+    # receptive fields of neurons in level 2
+    for i in range(model.level2_module_size):
         u2 = model.get_level2_rf(i)
         u2 = cv2.resize(u2, None, fx=4, fy=4, interpolation=cv2.INTER_NEAREST)
         imageio.imwrite("result/u2_{:0>3}.png".format(i), u2)
-        
+    
+    # difference-of-gaussian filtered image inputs
+    for i in range(len(dataset.filtered_images)):
+        filtered_img = dataset.filtered_images[i]
+        filtered_img = cv2.resize(filtered_img, None, fx=4, fy=4, interpolation=cv2.INTER_NEAREST)
+        imageio.imwrite("result/input_image{:0>2}.png".format(i), filtered_img)
+
+    # image reconstruction based on level 1 and level 2 representations
+    for i in range(len(dataset.images)):
+        images = dataset.get_images(i)
+        rs, r_tds, rh, error_tds = model.apply_images(images, training=False)
+        level1_img = model.reconstruct(rs, level=1)
+        level2_img = model.reconstruct(rh, level=2)
+        level1_img = cv2.resize(level1_img, None, fx=4, fy=4, interpolation=cv2.INTER_NEAREST)
+        level2_img = cv2.resize(level2_img, None, fx=4, fy=4, interpolation=cv2.INTER_NEAREST)
+        imageio.imwrite("result/level1_image{:0>2}.png".format(i), level1_img)
+        imageio.imwrite("result/level2_image{:0>2}.png".format(i), level2_img)
 
 if __name__ == '__main__':
     main()
